f7134e7f8f26795dfb8a6c5f379a73fe5be9969b
Aby Herwendo <ablehunder@abyMac.local>
2023-11-20 05:39:19 +0700

    final dosbing

--- js/ua.js
+++ js/ua.js
@@ -6,17 +6,17 @@ var themes = {
     'pink'      : 'Pinky'
 };
 
-var songs = [
-    '2meKbkpUpbg',
-    '5WGTCsk3KC4',
-    'brDjam33BfE',
-    'bSv4yesuaMQ',
-    'f4crKVutaS0',
-    'u3IydjQzJIg',
-    'VWEWJDJcA_A',
-    'W4toJlqM_50',
-    'YW1k9qhcUEI'
-];
+var songs = {
+    'W4toJlqM_50' : '조금씩 천천히',
+    'f4crKVutaS0' : 'Beyond My Dreams',
+    'u3IydjQzJIg' : 'First Love',
+    'brDjam33BfE' : 'Forever Love',
+    'YW1k9qhcUEI' : 'La Vie en Rose (Piano)',
+    'bSv4yesuaMQ' : 'Mahana',
+    '5WGTCsk3KC4' : 'Married Life (Piano)',
+    'VWEWJDJcA_A' : 'Married Life (Flute)',
+    '2meKbkpUpbg' : 'Picnic'
+};
 
 function getUrlVars()
 {
@@ -35,12 +35,10 @@ function getUrlVars()
 
 $(document).ready(function(){
 
-    // document.title= 'Comment this on production';
-
     var params=getUrlVars()
 
     var nama = (params && params['to'])? decodeURI(params['to']) : '';
-    // console.info(params, nama);
+    
     $('#namaundangan').text(nama);
     if (!nama || (nama && (nama.length==0))) $('#labelundangan').hide();
 
@@ -55,18 +53,13 @@ $(document).ready(function(){
                 $('#floatSetting, #fullpage').toggle();
         }
     });
-    $(window).on('popstate', function(event) {
-        // if ($('#floatSetting').is(":visible"))
-            // $('#floatSetting, #fullpage').toggle();
-    });
 
     // music button
     var playMusic = false;
     var songReady = false;
-    // var songReady = true;
     
     for (let k in songs) {
-        $('#songs').append($('<option>').val(songs[k]).text(songs[k]));
+        $('#songs').append($('<option>').val(k).text(songs[k]));
     } 
     var playNow = function(){
         playMusic = $('#music').is(':checked');
@@ -74,22 +67,11 @@ $(document).ready(function(){
         
         var bgSong = $('#bgSong').get(0);
 
-        console.info(bgSong);
-        if (bgSong)
-        {
-            console.info(songReady);
-            if (songReady){
-                console.info(playMusic);
-                if (playMusic) {
-                    // console.info('play');
+        if (bgSong && songReady){
+                if (playMusic) 
                     bgSong.play();
-                }
-                else {
-                    // console.info('pause');
+                else 
                     bgSong.pause();
-                }
-            }
-
         }
     }
     $('#songs').on('change', function(){        
@@ -100,12 +82,16 @@ $(document).ready(function(){
             playNow();
         }
     });
-    var randomSong = songs[Math.floor(Math.random() * songs.length)];
-    //$('#bgSong source').attr('src', 'songs/' + randomSong + '.mp3');
     $('#bgSong').get(0).addEventListener("canplay",function(){
         songReady = true;
     });
     
+    var r = Math.floor(Math.random() * Object.keys(songs).length);
+    var randomSong = null;
+    var c = 0;
+    for (let k in songs) {
+        if (c++ == r) randomSong = k;
+    }
     $('#songs').val(randomSong).change();
 
     $('#music').click(function(){ 
@@ -127,7 +113,7 @@ $(document).ready(function(){
         var bodyStyles = window.getComputedStyle(document.body);
         var themeColor = bodyStyles.getPropertyValue('--ua-pallete-3');
         $("#themeColor").attr('content', themeColor);
-        });
+    });
 
     var selectedTheme = params['theme'];
     if (!selectedTheme) selectedTheme = 'elegant';
@@ -141,7 +127,7 @@ $(document).ready(function(){
     // console.info('theme:', selectedTheme);
     $('#theme').val(selectedTheme).change();
 
-
+    // fullpage
     var myFullpage = new fullpage('#fullpage', {
         licenseKey: 'gplv3-license',
         anchors: ['front','love', 'time', 'quote'],
@@ -153,4 +139,86 @@ $(document).ready(function(){
         navigationPosition: 'left',
         css3: true        
     });
+
+    // lang
+    $('#lang').on('change', function(){
+        var ln = $(this).val();
+        i18next.changeLanguage(ln);
+        $('body').localize();
+    });
+    // use plugins and options as needed, for options, detail see
+    // https://www.i18next.com
+    i18next 
+    // detect user language
+    // learn more: https://github.com/i18next/i18next-browser-languageDetector
+    //.use(i18nextBrowserLanguageDetector)
+    // init i18next
+    // for all options read: https://www.i18next.com/overview/configuration-options
+    .init({
+        debug: true,
+        fallbackLng: 'id',
+        resources: {
+        id :{
+            translation: {
+                'home' : {
+                    'celebration': "Walimatul 'Urs",
+                    'greetName' : 'Yth. Bapak/Ibu/Saudara/i',
+                    'sorryName' : 'Mohon maaf jika ada kesalahan <span class="nowrap">penulisan nama dan gelar</span>'
+                },
+                'love'  : {
+                    'intro': 'Dengan memohon rahmat dan ridho <span class="nowrap">Allah SWT</span> kami mengharapkan kehadiran Bapak/Ibu/Saudara/i pada <span class="nowrap">Walimatul \'Urs</span>:',
+                    'parentBride': 'Putri dari <br><span class="nowrap">Bapak Abdul Kadir Sobur</span> <br>dan <span class="nowrap">Ibu Rosmala</span>',
+                    'parentGroom': 'Putra dari <br><span class="nowrap">Bapak M. Yassir Aragil (alm.)</span> <br>dan <span class="nowrap">Ibu Susyandari</span>'
+                },
+                'time'  : {'place': 'Ahad<br>3 Desember, 2023<br>Gedung LPMP Jambi', 'akad': '07.00 - 08.00 WIB', 'akadHeading': 'Akad Nikah', 'resepsi': '10.00 WIB - selesai', 'resepsiHeading': 'Resepsi'},
+                'quote' : {'translation': 'Dan segala sesuatu Kami ciptakan berpasang-pasangan agar kamu mengingat (kebesaran Allah)'}
+            }
+        },
+        en: {
+            translation: {
+                'home' : {
+                    'celebration': 'The Wedding of',
+                    'greetName' : 'Dear Mr/Mrs/Ms',
+                    'sorryName' : "We'd like to apologize for name and title spelling error."
+                },
+                'love'  : {
+                    'intro': 'You are cordially invited to <span class="nowrap">Wedding Celebration</span>:',
+                    'parentBride': 'Daughter of <span class="nowrap">Mr. Abdul Kadir Sobur</span> <br>and <span class="nowrap">Mrs. Rosmala</span>',
+                    'parentGroom': 'Son of <br><span class="nowrap">Mr. M. Yassir Aragil (alm.)</span> <br>dan <span class="nowrap">Mrs. Susyandari</span>'
+                },
+                'time'  : {'place': 'Sunday<br>December 3rd, 2023<br>LPMP Jambi Building', 'akad': '07.00 - 08.00 GMT+7', 'akadHeading': 'Ceremonial', 'resepsi': 'start at 10.00 GMT+7', 'resepsiHeading': 'Banquet'},
+                'quote' : {'translation': 'And We created pairs of all things so perhaps you would be mindful'}
+            }
+        },
+        jv : {
+            translation: {
+                'home' : {
+                    'celebration': "Walimatul 'Urs",
+                    'greetName' : 'Ingkang kinurmatan Bapak/Ibu/Sedherek',
+                    'sorryName' : "Nyuwun pangapunten menawi wonten asma kaliyan irah-irahan ingkang lepat"
+                },
+                'love'  : {
+                    'intro': 'Nuwun kanthi lumunturing rahmating Gusti Allah SWT, keparenga kula ngaturi rawuh panjenengan dhumateng <span class="nowrap">Walimatul \'Urs</span>:',
+                    'parentBride': 'Putri <br><span class="nowrap">Bapak Abdul Kadir Sobur</span> <br>kaliyan <span class="nowrap">Ibu Rosmala</span>',
+                    'parentGroom': 'Putra <br><span class="nowrap">Bapak M. Yassir Aragil (alm.)</span> <br>kaliyan <span class="nowrap">Ibu Susyandari</span>'
+                },
+                'time'  : {'place': 'Ahad<br>3 Desember, 2023<br>Gedung LPMP Jambi', 'akad': '07.00 - 08.00 WIB', 'akadHeading': 'Akad Nikah', 'resepsi': '10.00 WIB - rampung', 'resepsiHeading': 'Resepsi'},
+                'quote' : {'translation': 'Lan samubarang iku padha Ingsun titahaké ajodhon-jodhon, darapon sira padha angalap éling'}
+            }
+            
+
+        }
+        }
+    }, (err, t) => {
+        if (err) return console.error(err);
+
+        // for options see
+        // https://github.com/i18next/jquery-i18next#initialize-the-plugin
+        jqueryI18next.init(i18next, $, { useOptionsAttr: true });
+
+        // start localizing, details:
+        // https://github.com/i18next/jquery-i18next#usage-of-selector-function
+        $('body').localize();
+    });
+
 });
